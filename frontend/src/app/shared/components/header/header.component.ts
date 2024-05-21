29742bbcaefc44171b22fbf034856489c51fa74d
Marek Borecki <marek.borecki@finago.com>
2024-05-21 13:23:48 +0300

    add styles to components

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {FlexModule} from "@angular/flex-layout";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-header',
   standalone: true,
     imports: [
         RouterLink,
-        FlexModule
+        FlexModule,
+        MatIcon,
+        MatButton
     ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+    title: string = "Shop";
 
 }
--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatListOption, MatSelectionList} from "@angular/material/list";
+import {NgForOf} from "@angular/common";
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [
+        MatSelectionList,
+        MatListOption,
+        NgForOf
+    ],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+    categories: string[] = ["Kategoria 1", "Kategoria 2", "Kategoria 3", "Kategoria 4", "Kategoria 5"];
 
 }
