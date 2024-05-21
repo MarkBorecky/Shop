8f640cb125aa1d32fb5b1c7b6b49622a6056afaa
Marek Borecki <marek.borecki@finago.com>
2024-05-21 14:20:51 +0300

    add products view

--- frontend/src/app/modules/product/product.component.ts
+++ frontend/src/app/modules/product/product.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatCard, MatCardActions, MatCardContent, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+import {FlexModule} from "@angular/flex-layout";
+import {NgForOf} from "@angular/common";
+import {ProductService} from "./product.service";
+import {Product} from "./model/product";
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+    imports: [
+        MatCard,
+        MatCardTitle,
+        MatCardSubtitle,
+        MatCardActions,
+        MatButton,
+        MatCardContent,
+        FlexModule,
+        NgForOf
+    ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+    products: Product[] = [];
 
+    ngOnInit(): void {
+        this.getProducts();
+    }
+
+    constructor(private service : ProductService) {}
+
+    getProducts() {
+        this.products = this.service.getProducts();
+    }
 }
